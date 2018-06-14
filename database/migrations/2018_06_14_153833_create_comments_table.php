c94155d68fe500aa49c7eeb342aa93f74726dbf2
Zharkov Andrey <zharkov.php@gmail.com>
2018-06-14 18:57:53 +0300

    Create Migration and Model|| Создания Миграций и Моделей

--- database/migrations/2018_06_14_153833_create_comments_table.php
+++ database/migrations/2018_06_14_153833_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email');
-            $table->string('password');
-            $table->integer('is_admin')->default(0);
+            $table->text('text');
+            $table->integer('user_id');
+            $table->integer('post_id');
             $table->integer('status')->default(0);
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 }
