bd0307381e8fc1df3528caaf51d845a934a374d3
Zharkov Andrey <zharkov.php@gmail.com>
2018-06-16 13:24:54 +0300

    Загрузил стили в public, Webpack пока не работает, не смог подкгючить

---
+++ resources/views/admin/layout.blade.php
@@ -0,0 +1,15 @@
+
+    @include('admin.header')
+
+
+
+
+    <!-- Content Wrapper. Contains page content -->
+    @yield('content')
+    <!-- /.content-wrapper -->
+
+
+
+
+    @include('admin.footer')
+
