d6cf95ff4c1b91bc34fe0975d8a94293521dbe40
Zharkov Andrey <zharkov.php@gmail.com>
2018-06-21 18:38:08 +0300

    Factory for DB | Заполнение БД рандомно

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 use Faker\Generator as Faker;
 
+
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -19,5 +20,31 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'content' => $faker->sentence,
+        'image' => 'photo1.png',
+        'date' => '08/09/17',
+        'views'	=>	$faker->numberBetween(0, 5000),
+        'category_id'	=>	1,
+        'user_id'	=>	1,
+        'status'	=>	1,
+        'is_featured'	=>	0,
+    ];
+});
+
+$factory->define(App\Category::class, function (Faker $faker) {
+    return [
+        'title' => $faker->word,
+    ];
+});
+
+$factory->define(App\Tag::class, function (Faker $faker) {
+    return [
+        'title' => $faker->word,
     ];
 });
