0e74119b422008c77915b300c6e0f4155424456c
Zharkov Andrey <zharkov.php@gmail.com>
2018-06-25 14:48:10 +0300

    CRUD Подписчики в админке

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,7 @@ Route::group(['prefix'=>'admin','namespace'=>'Admin', 'middleware'	=>	'admin'], 
     Route::get('/comments/toggle/{id}', 'CommentsController@toggle');
     Route::delete('/comments/{id}/destroy', 'CommentsController@destroy')->name('comments.destroy');
 
+    Route::resource('/subscribers', 'SubscribersController');
 
 
 
